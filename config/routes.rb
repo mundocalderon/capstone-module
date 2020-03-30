929835f29ed430db445a5081d137e2b8bbac1096
mundocalderon-imac <mundocalderon@gmail.com>
2020-03-29 18:16:19 -0700

    installing devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'auth/whoami'
+
+  get 'auth/checkme'
+
+  mount_devise_token_auth_for 'User', at: 'auth'
   scope :api, defaults: {format: :json} do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
