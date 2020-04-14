af5db77b1744cd42f990f162686b9de3fe9b6c72
mundocalderon-imac <mundocalderon@gmail.com>
2020-04-13 18:04:24 -0700

    initializing user roles, starting with authorizing in images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :roles, inverse_of: :user, dependent: :destroy
+
+  def has_role(role_list, mname=nil, mid=nil)
+    role_names=roles.relevant(mname, mid).map {|r| r.role_name}
+    (role_names & role_list).any?
+  end
+
 end
