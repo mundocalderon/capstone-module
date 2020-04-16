10f48944751f179794db4c7f199126889feb0a03
mundocalderon-imac <mundocalderon@gmail.com>
2020-04-15 22:38:41 -0700

    authz things started

---
+++ app/policies/thing_policy.rb
@@ -0,0 +1,31 @@
+class ThingPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+  def show?
+    true
+  end
+  def create?
+    originator?
+  end
+  def update?
+    organizer?
+  end
+  def destroy?
+    organizer_or_admin?
+  end
+
+  class Scope < Scope
+    def user_roles
+      joins_clause=["left join Roles r on r.mname='Thing'",
+                    "r.mid=Things.id",
+                    "r.user_id #{user_criteria}"].join(" and ")
+      scope.select("Things.*, r.role_name")
+           .joins(joins_clause)
+    end
+
+    def resolve
+      user_roles
+    end
+  end
+end
