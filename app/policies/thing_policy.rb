86921be6c420abe22aee0bf0e15d239786cdeb67
mundocalderon-imac <mundocalderon@gmail.com>
2020-04-19 17:24:34 -0700

    ThingImage authorization

--- app/policies/thing_policy.rb
+++ app/policies/thing_policy.rb
@@ -15,14 +15,49 @@ class ThingPolicy < ApplicationPolicy
     organizer_or_admin?
   end
 
+  def get_linkables?
+    true
+  end
+
+  def get_images?
+    true
+  end
+
+  def add_image?
+    member_or_organizer?
+  end
+
+  def update_image?
+    organizer?
+  end
+
+  def remove_image?
+    organizer_or_admin?
+  end
+
   class Scope < Scope
-    def user_roles
-      joins_clause=["left join Roles r on r.mname='Thing'",
+    # def user_roles
+    #   joins_clause=["join Roles r on r.mname='Thing'",
+    #                 "r.mid=Things.id",
+    #                 "r.user_id #{user_criteria}"].join(" and ")
+    #   scope.select("Things.*, r.role_name")
+    #        .joins(joins_clause)
+    #        .where("r.role_name"=>[Role::ORGANIZER, Role::MEMBER])
+    # end
+
+    def user_roles members_only=true
+      member_join = members_only ? "join" : "left join"
+      joins_clause=["#{member_join} Roles r on r.mname='Thing'",
                     "r.mid=Things.id",
                     "r.user_id #{user_criteria}"].join(" and ")
       scope.select("Things.*, r.role_name")
            .joins(joins_clause)
+           .tap {|s|
+             if members_only
+               s.where("r.role_name"=>[Role::ORGANIZER, Role::MEMBER])
+             end}
     end
+
 
     def resolve
       user_roles
