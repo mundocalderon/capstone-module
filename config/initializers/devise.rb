74936433a8aa4584434018ee5e09f65a72c54689
mundocalderon-imac <mundocalderon@gmail.com>
2020-04-21 23:17:41 -0700

    had to add devisekey and set dburl env var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,6 @@
 Devise.setup do |config|
   #config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.navigational_formats = [:json]
+  config.secret_key = 'c47b2832f6b009018917413133d9da6fc730df0972b5f5d0b2b3d150c97dc241a3477c6704e3e7d467310996e5807554fcc0b1cc25566eb12e24ec089629f5d8'
+
 end
