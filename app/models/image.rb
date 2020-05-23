664c3a6a112674b450ffe446715fbe6c323d9b5d
mundocalderon-mbp <mundocalderon@gmail.com>
2020-05-22 17:36:27 -0700

    added Point ValueObject class

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,6 +5,24 @@ class Image < ActiveRecord::Base
   has_many :thing_images, inverse_of: :image, dependent: :destroy
   has_many :things, through: :thing_images
 
+
+=begin
+
+Adds reader and writer methods for manipulating a value object: 
+composed_of :address adds address and address=(new_address) methods.
+
+The first item is the attribute name in the ActiveRecord model, 
+and the second is the name of the attribute in the ValueObject 
+(the writer uses it to read from the VO).
+
+Furthermore, the order in which mapping pairs are specified should 
+be the same as the order the attributes are specified in the 
+ValueObject’s initialize method (for the reader to be able to 
+instantiate a VO with the record’s values).
+  
+=end
+  composed_of :position, class_name: "Point", allow_nil: true, mapping: [%w(lng lng), %w(lat lat)]
+
   def basename
   	caption || "image-#{id}"
   end
