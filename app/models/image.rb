14aa2c3c37c29752865600abd60f046ae2eea6a3
mundocalderon-mbp <mundocalderon@gmail.com>
2020-05-22 17:48:28 -0700

    adding Geokit rails to Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,25 +5,31 @@ class Image < ActiveRecord::Base
   has_many :thing_images, inverse_of: :image, dependent: :destroy
   has_many :things, through: :thing_images
 
+	# Adds reader and writer methods for manipulating a value object: 
+	# composed_of :address adds address and address=(new_address) methods.
 
-=begin
+	# The first item is the attribute name in the ActiveRecord model, 
+	# and the second is the name of the attribute in the ValueObject 
+	# (the writer uses it to read from the VO).
 
-Adds reader and writer methods for manipulating a value object: 
-composed_of :address adds address and address=(new_address) methods.
-
-The first item is the attribute name in the ActiveRecord model, 
-and the second is the name of the attribute in the ValueObject 
-(the writer uses it to read from the VO).
-
-Furthermore, the order in which mapping pairs are specified should 
-be the same as the order the attributes are specified in the 
-ValueObject’s initialize method (for the reader to be able to 
-instantiate a VO with the record’s values).
-  
-=end
+	# Furthermore, the order in which mapping pairs are specified should 
+	# be the same as the order the attributes are specified in the 
+	# ValueObject’s initialize method (for the reader to be able to 
+	# instantiate a VO with the record’s values).
   composed_of :position, class_name: "Point", allow_nil: true, mapping: [%w(lng lng), %w(lat lat)]
+  acts_as_mappable
+
+  def to_lat_lng
+  	Geokit::LatLng.new(lat,lng)
+  end
 
   def basename
   	caption || "image-#{id}"
   end
+end
+
+Point.class_eval do
+	def to_lat_lng
+		Geokit::LatLng.new(*latlng)
+	end
 end
