99fd662257e975e864522bd059af92885aefbdd8
mundocalderon-mbp <mundocalderon@gmail.com>
2020-05-24 12:02:51 -0700

    cache check with eTag build from the last_modified

--- app/controllers/thing_images_controller.rb
+++ app/controllers/thing_images_controller.rb
@@ -16,17 +16,25 @@ class ThingImagesController < ApplicationController
   end
 
   def subjects
+    expires_in 1.minute, :public =>true
     miles=params[:miles] ? params[:miles].to_f : nil
     subject=params[:subject]
     distance=params[:distance] ||= "false"
     reverse=params[:order] && params[:order].downcase=="desc"  #default to ASC
-    @thing_images=ThingImage.within_range(@origin, miles, reverse)
-      .with_name
-      .with_caption
-      .with_position
-    @thing_images=@thing_images.things    if subject && subject.downcase=="thing"
-    @thing_images=ThingImage.with_distance(@origin, @thing_images) if distance.downcase=="true"
-    render "thing_images/index"
+    last_modified = ThingImage.last_modified
+    state="#{request.headers['QUERY_STRING']}:#{last_modified}"
+    #use eTag versus last_modified -- ng-token-auth edits if-modified-since
+    eTag="#{Digest::MD5.hexdigest(state)}"
+
+    if stale? :etag => eTag
+      @thing_images=ThingImage.within_range(@origin, miles, reverse)
+        .with_name
+        .with_caption
+        .with_position
+      @thing_images=@thing_images.things    if subject && subject.downcase=="thing"
+      @thing_images=ThingImage.with_distance(@origin, @thing_images) if distance.downcase=="true"
+      render "thing_images/index"
+    end
   end  
 
   def image_things
@@ -81,6 +89,7 @@ class ThingImagesController < ApplicationController
 
   def destroy
     authorize @thing, :remove_image?
+    @thing_image.image.touch #image will be only thing left 
     @thing_image.destroy
     head :no_content
   end
@@ -115,7 +124,7 @@ class ThingImagesController < ApplicationController
         @origin=Point.new(params[:lng].to_f, params[:lat].to_f)
       else
         raise ActionController::ParameterMissing.new(
-          "an origin [lng/lat or address required")
+          "an origin [lng/lat] required")
       end
     end    
 end
