bfd05f7045e5496cc18050cf141fc101ba1fa770
mundocalderon-mbp <mundocalderon@gmail.com>
2020-05-31 21:00:28 -0700

    completed current image viewer

--- app/assets/javascripts/capstone/subjects/current_images/current_images.component.js
+++ app/assets/javascripts/capstone/subjects/current_images/current_images.component.js
@@ -6,13 +6,26 @@
     .component("capCurrentImages", {
       templateUrl: imagesTemplateUrl,
       controller: CurrentImagesController,
-    });
+    })
+    .component("capCurrentImageViewer", {
+      templateUrl: imageViewerTemplateUrl,
+      controller: CurrentImageViewerController,
+      bindings: {
+        name: "@",
+        minWidth: "@"
+      }
+    })
+    ;
 
 
   imagesTemplateUrl.$inject = ["capstone.config.APP_CONFIG"];
   function imagesTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.current_images_html;
   }    
+  imageViewerTemplateUrl.$inject = ["capstone.config.APP_CONFIG"];
+  function imageViewerTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.current_image_viewer_html;
+  }  
 
   CurrentImagesController.$inject = ["$scope",
                                      "capstone.subjects.currentSubjects"];
@@ -34,6 +47,33 @@
     //////////////
     
     function imageClicked(index) {
+      currentSubjects.setCurrentImage(index);
+    }
+  }
+
+  CurrentImageViewerController.$inject = ["$scope",
+                                          "capstone.subjects.currentSubjects"];
+  function CurrentImageViewerController($scope, currentSubjects) {
+    var vm=this;
+    vm.viewerIndexChanged = viewerIndexChanged;
+
+    vm.$onInit = function() {
+      console.log("CurrentImageViewerController",$scope);
+    }
+    vm.$postLink = function() {
+      $scope.$watch(
+        function() { return currentSubjects.getImages(); }, 
+        function(images) { vm.images = images; }
+      );
+      $scope.$watch(
+        function() { return currentSubjects.getCurrentImageIndex(); }, 
+        function(index) { vm.currentImageIndex = index; }
+      );
+    }    
+    return;
+    //////////////
+    function viewerIndexChanged(index) {
+      console.log("viewer index changed, setting currentImage", index);
       currentSubjects.setCurrentImage(index);
     }
   }
